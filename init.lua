081ec7d357c07d9bc1ee3c77585ba782faca0686
Antonio G <nio@Antonios-MacBook-Pro.local>
2021-11-16 18:39:29 +0000

    First commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+--[[
+
+  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--]]
+
+-----------------------------------------------------------
+-- Import Lua modules
+-----------------------------------------------------------
+require('settings')
+require('keymaps')
+require('plugins/packer')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-autopairs')
+require('plugins/feline')
+require('plugins/vista')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
