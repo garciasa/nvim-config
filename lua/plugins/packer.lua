de5c308884cb92aa0eaeb012017ea2437cf78d80
garciasa <garciasa@gmail.com>
2022-03-27 16:59:05 +0100

    last changes

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -73,9 +73,6 @@ return packer.startup(function()
   use {
     'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('gitsigns').setup()
-    end,
   }
   -- telescope
   use {
